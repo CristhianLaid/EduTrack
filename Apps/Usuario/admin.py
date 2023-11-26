ecf576a74257d815c61b50709e3538e2d108dfd4
CristhianLaid <cmurillos@unemi.edu.ec>
2023-11-26 13:45:23 -0500

    Se agrega los models y los registros en el admin para Datos_institucionales.

--- Apps/Usuario/admin.py
+++ Apps/Usuario/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Rol, Usuario
+from .models import Rol, Usuario, Facultad
 
 # Register your models here.
 admin.site.register(Rol)
 admin.site.register(Usuario)
+admin.site.register(Facultad)
--- Apps/Usuario/models.py
+++ Apps/Usuario/models.py
@@ -12,6 +12,15 @@ class Rol(models.Model):
     class Meta:
         db_table = 'TipoUsuario'
 
+class Facultad(models.Model):
+    nombreFacultad = models.CharField(max_length=100, blank=False, null=True)
+
+    def __str__(self):
+        return f'{self.nombreFacultad}'
+    class Meta:
+        db_table = 'Facultad'
+
+
 class Usuario(models.Model):
     usuario = models.OneToOneField(User, on_delete=models.CASCADE)
     rol = models.ForeignKey(Rol, on_delete=models.CASCADE)
@@ -19,9 +28,10 @@ class Usuario(models.Model):
     telefono = models.CharField(max_length=10, blank=True, null=True)
     cedula = models.CharField(max_length=10, blank=True, null=True)
     direccion = models.CharField(max_length=255, blank=True, null=True)
+    facultad = models.ForeignKey(Facultad, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
-        return f'${self.usuario} - ${self.rol}'
+        return f'{self.usuario} - {self.rol}'
 
     class Meta:
         db_table = 'Usuario'
